d01709684e2e44e895c6cd128a0448450da1cf01
Natalie Martell <nrmartell@gmail.com>
2016-01-18 15:44:40 -0700

    intro

---
+++ introductions.java
@@ -0,0 +1,10 @@
+import java.io.Console;
+	
+public class Introductions{
+
+	public static void main(String[] args) {
+		Console console = System.console();
+		console.printf("Hello my name is Natalie");
+	}
+
+}
