3ad5e32bfee9ccdeffbe7defdf6dd08436f68729
Natalie Martell <nrmartell@gmail.com>
2016-01-18 15:56:00 -0700

    intro 2

--- introductions.java
+++ introductions.java
@@ -3,8 +3,11 @@ import java.io.Console;
 public class Introductions{
 
 	public static void main(String[] args) {
+
+		String firstName= "Sam";
 		Console console = System.console();
-		console.printf("Hello my name is Natalie");
+		console.printf("Hello my name is %s\n", firstName);
+		console.printf("%s is learning java!\n", firstName);
 	}
 
 }
