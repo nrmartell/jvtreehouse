ae1ccad58b7bd111f28059dce4573f39f066ede3
Natalie Martell <nrmartell@gmail.com>
2016-01-18 16:17:48 -0700

    new files

---
+++ intro/input.java
@@ -0,0 +1,10 @@
+import java.io.Console;
+	
+public class Input{
+
+	public static void main(String[] args) {
+
+		String firstName= console.readLine("What is your name? ");
+		Console console = System.console();
+	}
+}
