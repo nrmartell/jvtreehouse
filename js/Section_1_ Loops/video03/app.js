c6fa17b5dbfe3dff17245959e295dedb49ab727a
Natalie Martell <nrmartell@gmail.com>
2016-01-21 14:52:09 -0700

    js objectives

---
+++ js/Section_1_ Loops/video03/app.js
@@ -0,0 +1,17 @@
+var upper = 10000;
+var randomNumber = getRandomNumber(upper);
+var guess;
+var attempts = 0;
+
+function getRandomNumber(upper) {
+  return Math.floor( Math.random() * upper ) + 1;
+}
+
+while (guess !== randomNumber){
+	guess = getRandomNumber(upper);
+	attempts += 1;
+
+}
+
+document.write(" <p> The random number was:" + randomNumber + "<p>");
+document.write("<p> It took th computer "+ attempts + "attempts to get it. </p>");
