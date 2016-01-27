eaeb55415ac47843d7355413f555ae057957fcb9
Natalie Martell <nrmartell@gmail.com>
2016-01-27 13:30:43 -0700

    new exercises

--- js/Section_3_Objects/video04/js/quiz.js
+++ js/Section_3_Objects/video04/js/quiz.js
@@ -1,7 +1,7 @@
 var questions = [
-  ['How many states are in the United States?', 50],
-  ['How many continents are there?', 7],
-  ['How many legs does an insect have?', 6]
+  { question:'How many states are in the United States?', answer: 50},
+  {question:'How many continents are there?', answer: 7},
+  {question:'How many legs does an insect have?', answer: 6}
 ];
 var correctAnswers = 0;
 var question;
